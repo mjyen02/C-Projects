5a6ce7b9fdccebec247b278a96148d517753bca0
mjyen02 <136381221+mjyen02@users.noreply.github.com>
2026-08-24 14:18:06 -0500

    Add files via upload

---
+++ Pool Maintenance Router/Location.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECE2310_Final_Project
+{
+    internal class Location
+    {
+        private int x;
+        private int y;
+
+        public Location()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Location(int xVal)
+        {
+            x = xVal;
+            y = 0;
+        }
+
+        public Location(int xVal, int yVal)
+        {
+            x = xVal;
+            y = yVal;
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public override string ToString()
+        {
+            return "Pool "+ "is located at " + x.ToString() + ", " + y.ToString();
+        }
+
+        public double findDistance(Location location2)
+        {
+            double distance = Math.Round(Math.Sqrt(Math.Pow(location2.X - x, 2) + Math.Pow(location2.Y - y, 2)));
+            return distance;
+        }
+
+        ~Location() { }
+    }
+}
